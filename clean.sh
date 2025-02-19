f234b62ed0cfee24edf3cf6f1183a3f5627fa57a
David Gutierrez <david68cu@hotmail.com>
2025-02-19 13:49:38 +0000

    Assigment 4 part 2 completed

---
+++ clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script to clean the Buildroot build directory
+# Author: Siddhant Jajoo
+
+# Navigate to the Buildroot directory
+cd buildroot
+
+# Run make distclean to clean the build directory
+make distclean
+
