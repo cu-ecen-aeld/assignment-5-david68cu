ef6257212b5164c562384055bf227fc1e12de9d5
David Gutierrez <david68cu@hotmail.com>
2025-02-27 00:18:23 +0000

    Fix wronn directories

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -19,6 +19,7 @@ define AESD_ASSIGNMENTS_BUILD_CMDS
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
@@ -28,9 +29,6 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/conf/
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/assignment5/* $(TARGET_DIR)/assignment-autotest/assignment5/
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/assignment5-buildroot/* $(TARGET_DIR)/assignment-autotest/assignment5-buildroot/
 endef
 
 $(eval $(generic-package))
